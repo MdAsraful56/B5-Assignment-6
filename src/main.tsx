0e3147cd11bae0604b6cecc2a52d0687ec82f32e
MdAsraful56 <work.mdasraful56@gmail.com>
2025-08-21 00:21:16 +0600

    redux all setup , some page, layput base

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
 import { RouterProvider } from 'react-router';
-import App from './App.tsx';
+import { Toaster } from 'sonner';
 import './index.css';
 import { ThemeProvider } from './Provider/theme-provider.tsx';
+import { store } from './Redux/store.ts';
 import { router } from './Routes/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-            <RouterProvider router={router} />
-            <App />
-        </ThemeProvider>
+        <ReduxProvider store={store}>
+            <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+                <RouterProvider router={router} />
+                <Toaster richColors />
+            </ThemeProvider>
+        </ReduxProvider>
     </StrictMode>
 );
